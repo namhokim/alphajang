7ba58e2762f5bab1d8cdf9c5203236fbf997b49b
Raphael(김남호) <namo@coupang.com>
2016-03-11 21:51:42 +0900

    init xcode template of objective-c

---
+++ alphajang/alphajang/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  alphajang
+//
+//  Created by 김남호 on 2016. 3. 11..
+//  Copyright © 2016년 namo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ alphajang/alphajang/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  alphajang
+//
+//  Created by 김남호 on 2016. 3. 11..
+//  Copyright © 2016년 namo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
